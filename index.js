5c834cfa60090fd27691b2e1fa6b742aa0ba06cc
victoriamoret <victoriamoret90@gmail.com>
2019-10-15 00:40:33 +0200

    nav and header changed

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ icon.onclick = function() {
     if (nav.style.visibility == "hidden") {
         nav.style.visibility = "visible";
         icon.innerHTML = "X";
+        nav.style.background = "white";
+        nav.style.transition = "0.5s ease-out";
         uls.style.transform = "translate(+50vw)";
         uls.style.transition = "0.5s ease-out";
     } else {
         uls.style.transform = "translate(-50vw)";
         uls.style.transition = "1s ease-out";
+        nav.style.background = "none";
+        nav.style.transition = "0.5s ease-out";
         nav.style.visibility = "hidden";
         icon.innerHTML = "=";
     }
