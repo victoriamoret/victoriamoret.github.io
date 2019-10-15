5ef0220668f752cba5756e8903e5df18ba85ab26
victoriamoret <victoriamoret90@gmail.com>
2019-10-15 15:32:49 +0200

    nav

--- index.js
+++ index.js
@@ -4,28 +4,18 @@ var uls = document.querySelector("nav ul");
 icon.style.visibility = "visible";
 nav.style.visibility = "hidden";
 
-icon.onmouseover = function show() {
+icon.onclick = function() {
     if (nav.style.visibility == "hidden") {
         nav.style.visibility = "visible";
         icon.innerHTML = "X";
-        // nav.style.background = "white";
-        nav.style.transition = "0.5s ease-out";
-        uls.style.transform = "translate(+50vw)";
-        uls.style.transition = "0.5s ease-out";
-    }
-};
-icon.onclick = function close() {
-    if (nav.style.visibility == "hidden") {
-        nav.style.visibility = "visible";
-        icon.innerHTML = "X";
-        // nav.style.background = "white";
+        nav.style.background = "white";
         nav.style.transition = "0.5s ease-out";
         uls.style.transform = "translate(+50vw)";
         uls.style.transition = "0.5s ease-out";
     } else {
         uls.style.transform = "translate(-50vw)";
         uls.style.transition = "1s ease-out";
-        // nav.style.background = "none";
+        nav.style.background = "none";
         nav.style.transition = "0.5s ease-out";
         nav.style.visibility = "hidden";
         icon.innerHTML = "=";
