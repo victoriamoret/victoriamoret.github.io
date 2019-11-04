98fbd2fc8f242e352491edd897f37db3e792d094
victoriamoret <victoriamoret90@gmail.com>
2019-11-04 17:01:15 +0100

    removed comments

--- exercises/node-console-chalk/index.js
+++ exercises/node-console-chalk/index.js
@@ -47,32 +47,3 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(8080, () => ca.rainbow("listening"));
-
-//
-//
-// let server = http.createServer(function(request, response) {
-//     if (request.method == "GET") {
-//         response.statusCode = 200;
-//         response.setHeader("Content-type", "text/html");
-//         response.end(
-//             '<!doctype html><html><title>Colors</title><form method="POST"><input type="text" name="text"><select name="color"><option value="red">red</option><option value="blue">blue</option><option value="green">green</option><option value="yellow">yellow</option><option value="gray">gray</option><option value="magenta">magenta</option><option value="cyan">cyan</option></select><button type="submit">Go</button></form></html>'
-//         );
-//     }
-//     if (request.method == "POST") {
-//         var body = "";
-//         request
-//             .on("data", function(chunk) {
-//                 body += chunk;
-//             })
-//             .on("end", function() {
-//                 console.log(body);
-//                 let myBody = qs.parse(body);
-//                 console.log(myBody.text);
-//                 var color = myBody.color;
-//                 response.end("<h1>Hello " + myBody.text + " World</h1>");
-//             });
-//     }
-// });
-// server.listen(8080, () => {
-//     ca.rainbow("listening");
-// });
